3c7eee80d163819a44149300d6b661e465013306
Lukas Devos <ldevos98@gmail.com>
2025-02-26 17:18:19 -0500

    improve testing

--- test/test_basics.jl
+++ test/test_basics.jl
@@ -1,6 +1,6 @@
 using Test: @test, @testset, @test_throws
 using DerivableInterfaces: DerivableInterfaces as DI
-using DerivableInterfaces: @derive, @interface
+using DerivableInterfaces: @derive, @interface, AbstractArrayInterface
 
 # Test setup
 # ----------
@@ -12,8 +12,8 @@ Base.parent(A::MyArray) = A.parent
 @derive (T=MyArray,) Base.getindex(::T, ::Int)
 
 # Interfacetype
-struct MyInterface{N} <: DI.AbstractArrayInterface{N} end
-DI.interface(::Type{A}) where {A<:MyArray} = MyInterface{ndims(A)}()
+struct MyInterface <: DI.AbstractArrayInterface end
+DI.interface(::Type{A}) where {A<:MyArray} = MyInterface()
 
 const f_ctr = Ref(0) # used to verify if function was actually called
 @interface ::MyInterface function Base.getindex(A::MyArray, i::Int)
@@ -25,11 +25,9 @@ end
   return getindex(A::AbstractArray, i::Int)
 end
 
-f(A, B) = -1
-for N in 1:3
-  @eval @interface ::MyInterface{$N} f(A, B) = $N
-end
-@derive (T=AbstractArray,) f(::T, ::T)
+@derive (TA=Any, TB=Any) f(::TA, ::TB)
+@interface ::AbstractArrayInterface f(A, B) = -1
+@interface ::MyInterface f(A, B) = 1
 
 # Tests
 # -----
@@ -45,7 +43,7 @@ end
 @testset "using @interface functions for non-derived types" begin
   ctr = f_ctr[]
   A = zeros(Int, 3)
-  @test A[1] == @interface MyInterface{1}() A[1]
+  @test A[1] == @interface MyInterface() A[1]
   @test f_ctr[] == ctr + 1
 end
 
@@ -55,10 +53,9 @@ end
   @test f(zeros(1), zeros(1, 1)) == -1
   # MyInterface
   @test f(MyArray(zeros(1)), MyArray(zeros(1))) == 1
-  @test f(MyArray(zeros(1, 1)), MyArray(zeros(1, 1))) == 2
   # Mix
   @test f(MyArray(zeros(1)), zeros(1)) == 1
   @test f(zeros(1), MyArray(zeros(1))) == 1
   # undefined mix
-  @test f((1,), zeros(1)) == 1
+  @test_throws ErrorException f((1,), zeros(1)) == -1
 end
