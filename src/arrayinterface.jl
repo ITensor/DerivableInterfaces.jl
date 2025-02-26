fe8031b6730c155e5b9b054333f3a6fb60031058
Lukas Devos <ldevos98@gmail.com>
2025-02-26 17:18:13 -0500

    start adding fallbacks

--- src/arrayinterface.jl
+++ src/arrayinterface.jl
@@ -1,5 +1,6 @@
 """
 `AbstractArrayInterface <: AbstractInterface` is the abstract supertype for any interface
+using Base: BroadcastStyle
 associated with an `AbstractArray` type.
 """
 abstract type AbstractArrayInterface <: AbstractInterface end
@@ -25,6 +26,10 @@ function interface(::Type{B}) where {B<:Broadcast.AbstractArrayStyle}
   return DefaultArrayInterface()
 end
 
+function interface(::Type{B}) where {B<:Broadcast.Broadcasted}
+  return interface(Broadcast.BroadcastStyle(B))
+end
+
 # Combination rules
 # -----------------
 function combine_interface_rule(::DefaultArrayInterface, I::AbstractArrayInterface)
@@ -39,3 +44,28 @@ end
 
 # Fallback implementations
 # ------------------------
+# whenever we want to overload new interface implementations, we better have a fallback that
+# sends us back to the default implementation.
+
+@interface ::AbstractArrayInterface Base.getindex(A::AbstractArray, I...) = (
+  @inline; getindex(A, I...)
+)
+@interface ::AbstractArrayInterface Base.setindex!(A::AbstractArray, v, I...) = (
+  @inline; setindex!(A, v, I...)
+)
+
+@interface ::AbstractArrayInterface Base.similar(
+  A::AbstractArray, ::Type{T}, axes
+) where {T} = similar(A, T, axes)
+
+@interface ::AbstractArrayInterface Base.map(f, A::AbstractArray, As::AbstractArray...) =
+  map(f, A, As...)
+@interface ::AbstractArrayInterface Base.map!(f, A::AbstractArray, As::AbstractArray...) =
+  map!(f, A, As...)
+
+@interface ::AbstractArrayInterface Base.reduce(
+  op, A::AbstractArray, As::AbstractArray...
+) = reduce(op, A, As...)
+@interface ::AbstractArrayInterface Base.mapreduce(
+  f, op, A::AbstractArray, As::AbstractArray...
+) = mapreduce(f, op, A, As...)
