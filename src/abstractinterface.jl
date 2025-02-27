bd4cc7aae70d14268534b2a51efc910811d0f7b8
mtfishman <mfishman@flatironinstitute.org>
2025-02-27 16:25:58 -0500

    Fix combining interfaces

--- src/abstractinterface.jl
+++ src/abstractinterface.jl
@@ -2,24 +2,24 @@
 interface(x) = interface(typeof(x))
 # TODO: Define as `DefaultInterface()`.
 interface(::Type) = error("Interface unknown.")
-interface(x1, x_rest...) = combine_interfaces(x1, x_rest...)
+interface(x1, x_rest...) = combine_interfaces(interface(x1), interface.(x_rest)...)
 
 # Adapted from `Base.Broadcast.combine_styles`.
 # Get the combined interfaces of the input objects.
-function combine_interfaces(x1, x2, x_rest...)
-  return combine_interfaces(combine_interfaces(x1, x2), x_rest...)
+function combine_interfaces(inter1, inter2, inter_rest...)
+  return combine_interfaces(combine_interfaces(inter1, inter2), inter_rest...)
 end
-combine_interfaces(x1, x2) = combine_interface_rule(interface(x1), interface(x2))
-combine_interfaces(x) = interface(x)
+combine_interfaces(inter1, inter2) = combine_interface_rule(inter1, inter2)
+combine_interfaces(inter) = interface(inter)
 
 # Rules for combining interfaces.
 function combine_interface_rule(
-  interface1::Interface, interface2::Interface
+  inter1::Interface, inter2::Interface
 ) where {Interface}
-  return interface1
+  return inter1
 end
 # TODO: Define as `UnknownInterface()`.
-combine_interface_rule(interface1, interface2) = error("No rule for combining interfaces.")
+combine_interface_rule(inter1, inter2) = error("No rule for combining interfaces.")
 
 abstract type AbstractInterface end
 
