5aa5affd6e1fd4d416ebe14c4e8caf53c8305fc5
Matt Fishman <mtfishman@users.noreply.github.com>
2025-02-28 08:45:31 -0500

    Fix combining interfaces (#29)

--- src/abstractinterface.jl
+++ src/abstractinterface.jl
@@ -2,25 +2,31 @@
 interface(x) = interface(typeof(x))
 # TODO: Define as `DefaultInterface()`.
 interface(::Type) = error("Interface unknown.")
-interface(x1, x_rest...) = combine_interfaces(x1, x_rest...)
-
-# Adapted from `Base.Broadcast.combine_styles`.
-# Get the combined interfaces of the input objects.
-function combine_interfaces(x1, x2, x_rest...)
-  return combine_interfaces(combine_interfaces(x1, x2), x_rest...)
-end
-combine_interfaces(x1, x2) = combine_interface_rule(interface(x1), interface(x2))
-combine_interfaces(x) = interface(x)
-
-# Rules for combining interfaces.
-function combine_interface_rule(
-  interface1::Interface, interface2::Interface
-) where {Interface}
-  return interface1
-end
-# TODO: Define as `UnknownInterface()`.
-combine_interface_rule(interface1, interface2) = error("No rule for combining interfaces.")
+interface(x1, x_rest...) = combine_interfaces(interface(x1), interface.(x_rest)...)
 
 abstract type AbstractInterface end
 
 (interface::AbstractInterface)(f) = InterfaceFunction(interface, f)
+
+# Adapted from `Base.Broadcast.combine_styles`.
+# Get the combined interfaces of the input objects.
+function combine_interfaces(
+  inter1::AbstractInterface, inter2::AbstractInterface, inter_rest::AbstractInterface...
+)
+  return combine_interfaces(combine_interface_rule(inter1, inter2), inter_rest...)
+end
+function combine_interfaces(inter1::AbstractInterface, inter2::AbstractInterface)
+  return combine_interface_rule(inter1, inter2)
+end
+combine_interfaces(inter::AbstractInterface) = inter
+
+# Rules for combining interfaces.
+function combine_interface_rule(
+  inter1::Interface, inter2::Interface
+) where {Interface<:AbstractInterface}
+  return inter1
+end
+# TODO: Define as `UnknownInterface()`.
+function combine_interface_rule(inter1::AbstractInterface, inter2::AbstractInterface)
+  return error("No rule for combining interfaces.")
+end
