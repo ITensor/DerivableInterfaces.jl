55e0b88d89daa5f0886c1654dd6dd873f608bec8
mtfishman <mfishman@flatironinstitute.org>
2025-03-27 09:34:37 -0400

    Update logos

--- docs/make.jl
+++ docs/make.jl
@@ -12,9 +12,9 @@ makedocs(;
   authors="ITensor developers <support@itensor.org> and contributors",
   sitename="DerivableInterfaces.jl",
   format=Documenter.HTML(;
-    canonical="https://ITensor.github.io/DerivableInterfaces.jl",
+    canonical="https://itensor.github.io/DerivableInterfaces.jl",
     edit_link="main",
-    assets=["assets/favicon.ico"],
+    assets=["assets/favicon.ico", "assets/extras.css"],
   ),
   pages=["Home" => "index.md", "Reference" => "reference.md"],
 )
